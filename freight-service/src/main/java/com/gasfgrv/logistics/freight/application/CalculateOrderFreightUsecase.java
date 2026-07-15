1a6d6ce348207b0333b1a7f15a9447b8ad594831
Gustavo Silva <gustavo_almeida11@hotmail.com>
2026-07-15 12:32:16 -0300

    feat: add use case implementations for calculating and cancelling order freight

---
+++ freight-service/src/main/java/com/gasfgrv/logistics/freight/application/CalculateOrderFreightUsecase.java
@@ -0,0 +1,13 @@
+package com.gasfgrv.logistics.freight.application;
+
+import com.gasfgrv.logistics.freight.domain.models.order.Order;
+import com.gasfgrv.logistics.freight.domain.ports.in.CalculateOrderFreightUsecasePort;
+
+public class CalculateOrderFreightUsecase implements CalculateOrderFreightUsecasePort {
+
+    @Override
+    public void calculate(Order order) {
+
+    }
+
+}
---
+++ freight-service/src/main/java/com/gasfgrv/logistics/freight/application/CancellOrderFreightUsecase.java
@@ -0,0 +1,13 @@
+package com.gasfgrv.logistics.freight.application;
+
+import com.gasfgrv.logistics.freight.domain.models.order.Order;
+import com.gasfgrv.logistics.freight.domain.ports.in.CancellOrderFreightUsecasePort;
+
+public class CancellOrderFreightUsecase implements CancellOrderFreightUsecasePort {
+
+    @Override
+    public void cancelOrderFreight(Order order) {
+
+    }
+
+}
