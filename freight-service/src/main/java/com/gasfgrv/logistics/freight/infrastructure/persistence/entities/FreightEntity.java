c64a77cae9f820869fb6705fb14e118c4da939cc
Gustavo Silva <gustavo_almeida11@hotmail.com>
2026-07-19 23:55:09 -0300

    feat: add FreightEntity class for DynamoDB persistence

---
+++ freight-service/src/main/java/com/gasfgrv/logistics/freight/infrastructure/persistence/entities/FreightEntity.java
@@ -0,0 +1,56 @@
+package com.gasfgrv.logistics.freight.infrastructure.persistence.entities;
+
+import com.gasfgrv.logistics.freight.domain.models.enuns.FreightStatus;
+import com.gasfgrv.logistics.freight.domain.models.order.Order;
+import com.gasfgrv.logistics.freight.infrastructure.persistence.converters.LocalDateTimeAttributeConverter;
+import lombok.Setter;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.*;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+@Setter
+@DynamoDbBean
+public class FreightEntity {
+
+    private UUID id;
+    private Order order;
+    private double distanceKm;
+    private BigDecimal price;
+    private FreightStatus status;
+    private Instant createdAt;
+
+    @DynamoDbPartitionKey
+    @DynamoDbAttribute("freight_id")
+    public UUID getId() {
+        return id;
+    }
+
+    @DynamoDbSortKey
+    @DynamoDbAttribute("order_id")
+    public Order getOrder() {
+        return order;
+    }
+
+    @DynamoDbAttribute("distance_km")
+    public double getDistanceKm() {
+        return distanceKm;
+    }
+
+    @DynamoDbAttribute("price")
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    @DynamoDbAttribute("status")
+    public FreightStatus getStatus() {
+        return status;
+    }
+
+    @DynamoDbAttribute("created_at")
+    @DynamoDbConvertedBy(LocalDateTimeAttributeConverter.class)
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+}
