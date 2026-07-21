b482889ba700dd96006ef12623842a53541a7830
Gustavo Silva <gustavo_almeida11@hotmail.com>
2026-07-20 21:38:44 -0300

    feat: enable configuration properties scanning in FreightServiceApplication

--- freight-service/src/main/java/com/gasfgrv/logistics/freight/FreightServiceApplication.java
+++ freight-service/src/main/java/com/gasfgrv/logistics/freight/FreightServiceApplication.java
@@ -2,8 +2,10 @@ package com.gasfgrv.logistics.freight;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class FreightServiceApplication {
 
     public static void main(String[] args) {
