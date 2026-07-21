142e987b56f1d65e35542abb0a6d6d51849f4e59
Gustavo Silva <gustavo_almeida11@hotmail.com>
2026-07-20 21:38:25 -0300

    feat: update FreightMapper to include order ID mapping for Freight and FreightEntity

--- freight-service/src/main/java/com/gasfgrv/logistics/freight/infrastructure/mappers/FreightMapper.java
+++ freight-service/src/main/java/com/gasfgrv/logistics/freight/infrastructure/mappers/FreightMapper.java
@@ -3,12 +3,15 @@ package com.gasfgrv.logistics.freight.infrastructure.mappers;
 import com.gasfgrv.logistics.freight.domain.models.freight.Freight;
 import com.gasfgrv.logistics.freight.infrastructure.persistence.entities.FreightEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface FreightMapper {
 
+    @Mapping(source = "order.id", target = "order")
     FreightEntity toEntity(Freight freight);
 
+    @Mapping(source = "order", target = "order.id")
     Freight toDomain(FreightEntity freightEntity);
 
 }
