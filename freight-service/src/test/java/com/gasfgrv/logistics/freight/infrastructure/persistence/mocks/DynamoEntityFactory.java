c49e283f3cca04bab6fc7e33880dd697c96a7012
Gustavo Silva <gustavo_almeida11@hotmail.com>
2026-07-22 13:34:30 -0300

    feat: add DynamoEntityFactory to build FreightEntity from Freight model

---
+++ freight-service/src/test/java/com/gasfgrv/logistics/freight/infrastructure/persistence/mocks/DynamoEntityFactory.java
@@ -0,0 +1,19 @@
+package com.gasfgrv.logistics.freight.infrastructure.persistence.mocks;
+
+import com.gasfgrv.logistics.freight.domain.models.freight.Freight;
+import com.gasfgrv.logistics.freight.infrastructure.persistence.entities.FreightEntity;
+
+public class DynamoEntityFactory {
+
+    public static FreightEntity buildEntity(Freight freight) {
+        FreightEntity entity = new FreightEntity();
+        entity.setId(freight.getId());
+        entity.setOrder(freight.getOrder().getId());
+        entity.setDistanceKm(freight.getDistanceKm());
+        entity.setPrice(freight.getPrice());
+        entity.setStatus(freight.getStatus());
+        entity.setCreatedAt(freight.getCreatedAt());
+        return entity;
+    }
+
+}
