e7f298c3788a5e843cdf5bd94a14fe3257804382
Gustavo Silva <gustavo_almeida11@hotmail.com>
2026-08-21 02:49:33 -0300

    feat(kafka): add test configuration for Kafka with Confluent Kafka and Schema Registry

--- freight-service/src/test/java/com/gasfgrv/logistics/freight/infrastructure/configurations/containers/KafkaTestcontainersConfiguration.java
+++ freight-service/src/test/java/com/gasfgrv/logistics/freight/infrastructure/configurations/containers/KafkaTestcontainersConfiguration.java
@@ -3,16 +3,60 @@ package com.gasfgrv.logistics.freight.infrastructure.configurations.containers;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.testcontainers.kafka.KafkaContainer;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.test.context.DynamicPropertyRegistrar;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.kafka.ConfluentKafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class KafkaTestcontainersConfiguration {
 
+    private static final String KAFKA_IMAGE = "confluentinc/cp-kafka:7.5.0";
+
+    private static final String SCHEMA_REGISTRY_IMAGE = "confluentinc/cp-schema-registry:7.5.0";
+
+    @Bean
+    public Network kafkaNetwork() {
+        return Network.newNetwork();
+    }
+
     @Bean
     @ServiceConnection
-    KafkaContainer kafkaContainer() {
-        return new KafkaContainer(DockerImageName.parse("apache/kafka-native:latest"));
+    @SuppressWarnings("resource")
+    public ConfluentKafkaContainer kafkaContainer(Network kafkaNetwork) {
+        return new ConfluentKafkaContainer(DockerImageName.parse(KAFKA_IMAGE))
+                .withNetwork(kafkaNetwork)
+                .withListener("kafka:19092");
+    }
+
+    @Bean
+    @DependsOn("kafkaContainer")
+    @SuppressWarnings("resource")
+    public GenericContainer<?> schemaRegistryContainer(ConfluentKafkaContainer kafkaContainer, Network kafkaNetwork) {
+        return new GenericContainer<>(DockerImageName.parse(SCHEMA_REGISTRY_IMAGE))
+                .withExposedPorts(8081)
+                .withNetwork(kafkaNetwork)
+                .withNetworkAliases("schema-registry")
+                .withEnv("SCHEMA_REGISTRY_HOST_NAME", "schema-registry")
+                .withEnv("SCHEMA_REGISTRY_KAFKASTORE_BOOTSTRAP_SERVERS", "PLAINTEXT://kafka:19092")
+                .withEnv("SCHEMA_REGISTRY_LISTENERS", "http://0.0.0.0:8081")
+                .waitingFor(Wait.forHttp("/subjects").forPort(8081).forStatusCode(200));
+    }
+
+    @Bean
+    public DynamicPropertyRegistrar dynamicPropertyRegistrar(ConfluentKafkaContainer kafkaContainer,
+                                                             GenericContainer<?> schemaRegistryContainer) {
+        return registry -> {
+            String schemaRegistryUrl = "http://%s:%d"
+                    .formatted(schemaRegistryContainer.getHost(), schemaRegistryContainer.getMappedPort(8081));
+            registry.add("spring.kafka.bootstrap-servers", kafkaContainer::getBootstrapServers);
+            registry.add("spring.kafka.properties.schema.registry.url", () -> schemaRegistryUrl);
+            registry.add("spring.kafka.producer.properties.schema.registry.url", () -> schemaRegistryUrl);
+            registry.add("spring.kafka.consumer.properties.schema.registry.url", () -> schemaRegistryUrl);
+        };
     }
 
 }
---
+++ freight-service/src/test/java/com/gasfgrv/logistics/freight/infrastructure/configurations/kafka/KafkaTestConfiguration.java
@@ -0,0 +1,66 @@
+package com.gasfgrv.logistics.freight.infrastructure.configurations.kafka;
+
+
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import org.apache.avro.specific.SpecificRecord;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.boot.kafka.autoconfigure.KafkaProperties;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.converter.MessagingMessageConverter;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.kafka.ConfluentKafkaContainer;
+
+import java.util.List;
+
+@EnableKafka
+@TestConfiguration(proxyBeanMethods = false)
+public class KafkaTestConfiguration {
+
+    @Bean
+    public KafkaProperties kafkaProperties(ConfluentKafkaContainer kafkaContainer, GenericContainer<?> schemaRegistryContainer) {
+        KafkaProperties properties = new KafkaProperties();
+        properties.setBootstrapServers(List.of(kafkaContainer.getBootstrapServers()));
+
+        String schemaRegistryUrl = "http://%s:%d"
+                .formatted(schemaRegistryContainer.getHost(), schemaRegistryContainer.getMappedPort(8081));
+        properties.getProperties().put("schema.registry.url", schemaRegistryUrl);
+
+        properties.getProducer().getProperties().put("schema.registry.url", schemaRegistryUrl);
+        properties.getProducer().getProperties().put("specific.avro.reader", "true");
+        properties.getProducer().getProperties().put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.getProducer().getProperties().put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class.getName());
+
+        properties.getConsumer().getProperties().put("schema.registry.url", schemaRegistryUrl);
+        properties.getConsumer().getProperties().put("specific.avro.reader", "true");
+        properties.getConsumer().getProperties().put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.getConsumer().getProperties().put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class.getName());
+        properties.getConsumer().getProperties().put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        return properties;
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, SpecificRecord> kafkaListenerContainerFactory(KafkaProperties properties) {
+        DefaultKafkaConsumerFactory<String, SpecificRecord> consumerFactory = new DefaultKafkaConsumerFactory<>(properties.buildConsumerProperties());
+        ConcurrentKafkaListenerContainerFactory<String, SpecificRecord> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        factory.setRecordMessageConverter(new MessagingMessageConverter());
+        return factory;
+    }
+
+    @Bean
+    public KafkaTemplate<String, SpecificRecord> kafkaTemplate(KafkaProperties properties) {
+        return new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(properties.buildProducerProperties()));
+    }
+
+}
